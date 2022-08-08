d6f304a320f65c4419761eb08761697c1648ba79
Matheus Yukio <matheusyukio.hocihara@gmail.com>
2022-08-08 16:33:26 -0300

    Conexao com o SQL server

---
+++ Models/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_CINEMA.Models
+{
+    public class Conexao: DbContext
+    {
+        public Conexao(DbContextOptions<Conexao> options): base(options) {
+        
+
+        }
+
+        public DbSet<Filmes> Filmes { get; set; }
+    }
+}
